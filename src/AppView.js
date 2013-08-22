9bc574d6a483d4b636ede4c827e73e21155a7816
Hack Reactor Students <github@hackreactor.com>
2013-08-22 12:30:31 -0700

    All tests passing, queue rendering to DOM.

--- src/AppView.js
+++ src/AppView.js
@@ -2,6 +2,7 @@ var AppView = Backbone.View.extend({
 
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+ //   this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
@@ -11,7 +12,8 @@ var AppView = Backbone.View.extend({
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      new LibraryView({collection: this.model.get('library')}).render()
+      new LibraryView({collection: this.model.get('library')}).render(),
+      new SongQueueView({collection: this.model.get('songQueue')}).render()
     ]);
   }
 
--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -1,12 +1,14 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.on('add', this.render());
+    this.on('remove', this.render());
   },
 
   tagName: "table",
 
   render: function() {
-  //  this.$el.children().detatch();
+    //this.$el.children().detatch();
     return this.$el.html('<th>Queue</th>').append(
       this.collection.map(function(song){
         return new SongQueueEntryView({model: song}).render();
