866b22848346a1e7e897eb299883a6b273941433
Hack Reactor Students <github@hackreactor.com>
2013-08-22 12:06:18 -0700

    added SongQueueView

--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -3,8 +3,15 @@ var SongQueueView = Backbone.View.extend({
   initialize: function() {
   },
 
+  tagName: "table",
+
   render: function() {
-    return this.$el;
+  //  this.$el.children().detatch();
+    return this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
